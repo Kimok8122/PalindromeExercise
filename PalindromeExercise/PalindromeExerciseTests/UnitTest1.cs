04d6ab1fdc4f3900a67d50ae44a277c111136987
Mona Lisa <email@example.com>
2020-09-14 13:53:39 -0600

    message

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("GoodDay", false)]
+        [InlineData("madam", true)]
+        [InlineData("Kimo",false)]
+
+        public void PalindromeTester(string word, bool expected)
+
         {
+            var wordSmith = new WordSmith();
+
+            var wordActual = wordSmith.IsAPalindrome(word);
+
+            Assert.Equal(expected, wordActual);
 
         }
     }
